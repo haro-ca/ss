02934f86cbaa03255f4af6d31947b91b8f596641
Ana Pau <grafapp@gmail.com>
2020-12-03 10:12:08 -0600

    Commit ejercicios diamond

--- curso_sat/src/cap11.R
+++
@@ -1,8 +0,0 @@
-# Librerías
-library(tidyverse)
-
-# Datos
-casas <- read_csv("data/raw/AmesHousing.csv")
-
-casas_pob <- casas %>% select(id, precio_miles, nombre_zona)
-casas_pob %>% sample_n(20) %>% formatear_tabla()
